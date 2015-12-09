16500543b58789d1c02640babbba76380bce0b48
Joel Klabo <joelklabo@gmail.com>
2015-12-09 01:13:03 -0800

    Parsing config file to Python Dictionary

---
+++ cfgtodict.py
@@ -0,0 +1,21 @@
+import configparser
+
+class ConfigToDictionary:
+
+  def __init__(self, config):
+    self.config = config
+
+  def dict(self):
+    # Parse config file
+    parser = configparser.ConfigParser()
+    parser.read(self.config)
+
+    # Set config values on Dictionary
+    dict = {}
+    
+    for section in parser.sections():
+      dict[section] = {}
+      for option in parser.options(section):
+        dict[section][option] = parser.get(section, option)
+
+    print(dict)
