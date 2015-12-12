10369893181cd12ab7c7201b09b2ed9bc18785a0
Joel Klabo <joelklabo@gmail.com>
2015-12-12 13:07:58 -0800

    adding configparser dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
   name='cfgtodict',
-  version='0.1.2',
+  version='0.1.3',
   description='A simple class for converting a *.cfg file to a Python Dictionary',
   url='https://github.com/joelklabo/cfgtodict',
   author='Joel Klabo',
   author_email='joelklabo@gmail.com',
   license='MIT',
-  classifiers = [
+  classifiers=[
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Development Status :: 4 - Beta",
@@ -16,5 +16,8 @@ setup(
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Topic :: Software Development :: Libraries"
+  ],
+  install_requires=[
+    'configparser'
   ]
 )
