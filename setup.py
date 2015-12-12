a5fe287c573d62a428aadb29c5031f0f063d4ad4
Joel Klabo <joelklabo@gmail.com>
2015-12-12 13:02:50 -0800

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,20 @@
-from setuptools import setup, find_packages
-
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the description from the readme.md
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
+from distutils.core import setup
 
 setup(
   name='cfgtodict',
-  version='0.1.0',
+  version='0.1.2',
   description='A simple class for converting a *.cfg file to a Python Dictionary',
-  long_description=long_description,
   url='https://github.com/joelklabo/cfgtodict',
   author='Joel Klabo',
   author_email='joelklabo@gmail.com',
   license='MIT',
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4'
-  ],
-  keywords='config dictionary utilities',
-  py_module=['cfgtodict']
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries"
+  ]
 )
